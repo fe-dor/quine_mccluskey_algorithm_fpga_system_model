1f21d33d9cc373f192810eb7532cda9ff3be65ec
fe-dor <parashchenko.fedor@gmail.com>
2023-09-27 23:54:42 +0300

    Update 1
    Склеивание автоматизировано. Проверено на 5 переменных.

--- Main.java
+++ Main.java
@@ -30,7 +30,7 @@ public class Main {
 
 
         //Группы
-        int[][][] groups0 = new int[k][k][n+1];
+        int[][][][] groups0 = new int[6][k][k][n+1];
 
 
         //Распределение по группам
@@ -41,52 +41,64 @@ public class Main {
         for(int i = 0; i < ci; i++){
             c = count_of_1(implicants[i]);
             for(int j = 0; j < n; j++){
-                groups0[c][cn[0][c]][j] = implicants[i][j];
+                groups0[0][c][cn[0][c]][j] = implicants[i][j];
             }
             cn[0][c]++;
         }
 
         //Группа с 4 переменными и одной *
         int local_count = 0;
-        int cmp_out = 0;
-        int[] local = new int[n+1];
-        int cmp_d = 0;
-        int cmp_u = 0;
-        boolean flag = true;
+        int cmp_out = 0; //result of compare two numbers
+        int[] local = new int[n+1]; //new number got by merging
+        int cmp_d = 0; //compare down
+        int cmp_u = 0; //compare up
+        boolean wf = true; //write flag
+        int ml = 0; //merging level
+        boolean cf = true; //comparing flag used to show that it was comparing on this comparing level
 
         int[][][] groups1 = new int[k][k*2][n+1];
         //Склеивание первой подгруппы внутри себя
-        for(cmp_u = 0; cmp_u < 5; cmp_u++) {
-            cmp_d = cmp_u + 1;
-            for (int i = 0; i < cn[0][cmp_u]; i++) {
-                for (int j = 0; j < cn[0][cmp_d]; j++) {
-                    cmp_out = compare_for_merging(groups0[cmp_u][i], groups0[cmp_d][j]); //больше 0 если можно склеить
-                    if (cmp_out >= 0) {
-                        local = groups0[cmp_u][i].clone();
-                        local[cmp_out] = 3;
-                        local[n] = 0;
 
-                        groups0[cmp_u][i][n] = 4;
-                        groups0[cmp_d][j][n] = 4;
+        while(cf) {
+            cf = false;
 
-                        c = count_of_1(local);
+            for (cmp_u = 0; cmp_u < 5; cmp_u++) {
+                cmp_d = cmp_u + 1;
+                for (int i = 0; i < cn[ml][cmp_u]; i++) {
+                    for (int j = 0; j < cn[ml][cmp_d]; j++) {
+                        cmp_out = compare_for_merging(groups0[ml][cmp_u][i], groups0[ml][cmp_d][j]); //больше 0 если можно склеить
+                        if (cmp_out >= 0) {
+                            cf = true;
+                            local = groups0[ml][cmp_u][i].clone();
+                            local[cmp_out] = 3;
+                            local[n] = 0;
 
-                        flag = true;
-                        for (int p = 0; p < cn[1][c]; p++) {
-                            if (compare_implicants(local, groups1[c][p])) {
-                                flag = false;
-                                break;
+                            groups0[ml][cmp_u][i][n] = 4;
+                            groups0[ml][cmp_d][j][n] = 4;
+
+                            c = count_of_1(local);
+
+                            wf = true;
+                            for (int p = 0; p < cn[ml + 1][c]; p++) {
+                                if (compare_implicants(local, groups0[ml + 1][c][p])) {
+                                    wf = false;
+                                    break;
+                                }
                             }
-                        }
 
-                        if (flag) {
-                            groups1[c][cn[1][c]] = local;
-                            cn[1][c]++;
+                            if (wf) {
+                                groups0[ml + 1][c][cn[ml + 1][c]] = local;
+                                cn[ml + 1][c]++;
+                            }
                         }
                     }
                 }
             }
+
+            ml++;
         }
+
+        /*
 
         int[][][] groups2 = new int[k][k*2][n+1];
         //Склеивание второй подгруппы внутри себя
@@ -105,15 +117,15 @@ public class Main {
 
                         c = count_of_1(local);
 
-                        flag = true;
+                        wf = true;
                         for (int p = 0; p < cn[2][c]; p++) {
                             if (compare_implicants(local, groups2[c][p])) {
-                                flag = false;
+                                wf = false;
                                 break;
                             }
                         }
 
-                        if (flag) {
+                        if (wf) {
                             groups2[c][cn[2][c]] = local;
                             cn[2][c]++;
                         }
@@ -139,15 +151,15 @@ public class Main {
 
                         c = count_of_1(local);
 
-                        flag = true;
+                        wf = true;
                         for (int p = 0; p < cn[3][c]; p++) {
                             if (compare_implicants(local, groups3[c][p])) {
-                                flag = false;
+                                wf = false;
                                 break;
                             }
                         }
 
-                        if (flag) {
+                        if (wf) {
                             groups3[c][cn[3][c]] = local;
                             cn[3][c]++;
                         }
@@ -157,6 +169,7 @@ public class Main {
         }
 
 
+*/
 
 
 
@@ -169,53 +182,18 @@ public class Main {
 //            }
 
 
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j < cn[0][i]; j++) {
-                for (int t = 0; t < n; t++) {
-                    System.out.print(groups0[i][j][t] + " ");
+        for(int r = 0; r < 6; r++) {
+            for (int i = 0; i <= n; i++) {
+                for (int j = 0; j < cn[r][i]; j++) {
+                    for (int t = 0; t < n; t++) {
+                        System.out.print(groups0[r][i][j][t] + " ");
+                    }
+                    System.out.print(" " + groups0[r][i][j][n]);
+                    System.out.println();
                 }
-                System.out.print(" " + groups0[i][j][n]);
                 System.out.println();
             }
-            System.out.println();
         }
-        System.out.println();
-        System.out.println();
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j < cn[1][i]; j++) {
-                for (int t = 0; t < n; t++) {
-                    System.out.print(groups1[i][j][t] + " ");
-                }
-                System.out.print(" " + groups1[i][j][n]);
-                System.out.println();
-            }
-            System.out.println();
-        }
-        System.out.println();
-        System.out.println();
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j < cn[2][i]; j++) {
-                for (int t = 0; t < n; t++) {
-                    System.out.print(groups2[i][j][t] + " ");
-                }
-                System.out.print(" " + groups2[i][j][n]);
-                System.out.println();
-            }
-            System.out.println();
-        }
-        System.out.println();
-        System.out.println();
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j < cn[3][i]; j++) {
-                for (int t = 0; t < n; t++) {
-                    System.out.print(groups3[i][j][t] + " ");
-                }
-                System.out.print(" " + groups3[i][j][n]);
-                System.out.println();
-            }
-            System.out.println();
-        }
-
 
     }
 
